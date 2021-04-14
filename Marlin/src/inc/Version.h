b6f3e004c315f55e065ad730cb2c8fea1fe9ff3b
Herman Rödström <herman@plejd.com>
2021-04-14 17:55:26 +0200

    Rename and show the custom name in "Info" page

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -82,7 +82,7 @@
  * providing the source code to your customers.)
  */
 #ifndef SOURCE_CODE_URL
-  #define SOURCE_CODE_URL "github.com/MarlinFirmware/Marlin"
+  #define SOURCE_CODE_URL "github.com/rodstrom/marlin"
 #endif
 
 /**
