59898f4ba864d0c45cb1fdcb2cfffe669473d0fb
Herman Rödström <herman@plejd.com>
2021-04-14 17:59:14 +0200

    Set bed size back to default 220x220x250

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1233,8 +1233,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 230
-#define Y_BED_SIZE 230
+#define X_BED_SIZE 220
+#define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
